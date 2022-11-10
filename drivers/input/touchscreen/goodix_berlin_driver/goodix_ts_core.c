9c3e4b6818e6f42565cef6c4709b80690d60695b
Wendly Li <wendlyli@google.com>
2022-11-10 18:31:06 +0000

    touch: goodix: Fix free input device twice
    Bug: 255257453
    Test: Run rmmod goodix_brl_touch; insmod goodix_brl_touch.ko
    Change-Id: Ib08e6e541b15e684c244ef95946e7b29631b96d2
    Signed-off-by: Wendly Li <wendlyli@google.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- drivers/input/touchscreen/goodix_berlin_driver/goodix_ts_core.c
+++ drivers/input/touchscreen/goodix_berlin_driver/goodix_ts_core.c
@@ -1525,9 +1525,6 @@ static int goodix_ts_input_dev_config(struct goodix_ts_core *core_data)
 		return -ENOMEM;
 	}
 
-	core_data->input_dev = input_dev;
-	input_set_drvdata(input_dev, core_data);
-
 	input_dev->name = GOODIX_CORE_DRIVER_NAME;
 	input_dev->phys = GOOIDX_INPUT_PHYS;
 	input_dev->id.product = 0xDEAD;
@@ -1568,6 +1565,9 @@ static int goodix_ts_input_dev_config(struct goodix_ts_core *core_data)
 		return r;
 	}
 
+	core_data->input_dev = input_dev;
+	input_set_drvdata(input_dev, core_data);
+
 	return 0;
 }
 
@@ -1582,9 +1582,6 @@ static int goodix_ts_pen_dev_config(struct goodix_ts_core *core_data)
 		ts_err("Failed to allocated pen device");
 		return -ENOMEM;
 	}
-
-	core_data->pen_dev = pen_dev;
-	input_set_drvdata(pen_dev, core_data);
 
 	pen_dev->name = GOODIX_PEN_DRIVER_NAME;
 	pen_dev->id.product = 0xDEAD;
@@ -1617,6 +1614,9 @@ static int goodix_ts_pen_dev_config(struct goodix_ts_core *core_data)
 		return r;
 	}
 
+	core_data->pen_dev = pen_dev;
+	input_set_drvdata(pen_dev, core_data);
+
 	return 0;
 }
 
@@ -1625,7 +1625,6 @@ void goodix_ts_input_dev_remove(struct goodix_ts_core *core_data)
 	if (!core_data->input_dev)
 		return;
 	input_unregister_device(core_data->input_dev);
-	input_free_device(core_data->input_dev);
 	core_data->input_dev = NULL;
 }
 
@@ -1634,7 +1633,6 @@ void goodix_ts_pen_dev_remove(struct goodix_ts_core *core_data)
 	if (!core_data->pen_dev)
 		return;
 	input_unregister_device(core_data->pen_dev);
-	input_free_device(core_data->pen_dev);
 	core_data->pen_dev = NULL;
 }
 
@@ -2604,35 +2602,36 @@ static int goodix_ts_remove(struct platform_device *pdev)
 	struct goodix_ts_hw_ops *hw_ops = core_data->hw_ops;
 	struct goodix_ts_esd *ts_esd = &core_data->ts_esd;
 
-	xiaomi_touch_deinit(core_data);
-	goodix_ts_unregister_notifier(&core_data->ts_notifier);
-	goodix_tools_exit();
-
 	if (core_data->init_stage >= CORE_INIT_STAGE2) {
+		hw_ops->irq_enable(core_data, false);
+		inspect_module_exit();
 	#ifdef GOODIX_SUSPEND_GESTURE_ENABLE
 		gesture_module_exit();
 	#endif
-		inspect_module_exit();
-		hw_ops->irq_enable(core_data, false);
 
+		core_module_prob_sate = CORE_MODULE_REMOVED;
+		if (atomic_read(&core_data->ts_esd.esd_on))
+			goodix_ts_esd_off(core_data);
+
+		goodix_ts_procfs_exit(core_data);
+		goodix_ts_sysfs_exit(core_data);
 	#if defined(CONFIG_DRM)
 		if (core_data->notifier_cookie)
 			panel_event_notifier_unregister(core_data->notifier_cookie);
 	#elif IS_ENABLED(CONFIG_FB)
 		fb_unregister_client(&core_data->fb_notifier);
 	#endif
-		core_module_prob_sate = CORE_MODULE_REMOVED;
-		if (atomic_read(&core_data->ts_esd.esd_on))
-			goodix_ts_esd_off(core_data);
-		goodix_ts_unregister_notifier(&ts_esd->esd_notifier);
+		goodix_ts_pen_dev_remove(core_data);
+		goodix_ts_input_dev_remove(core_data);
 
 		goodix_fw_update_uninit();
-		goodix_ts_input_dev_remove(core_data);
-		goodix_ts_pen_dev_remove(core_data);
-		goodix_ts_sysfs_exit(core_data);
-		goodix_ts_procfs_exit(core_data);
-		goodix_ts_power_off(core_data);
 	}
+
+	xiaomi_touch_deinit(core_data);
+	goodix_tools_exit();
+	goodix_ts_unregister_notifier(&ts_esd->esd_notifier);
+	goodix_ts_unregister_notifier(&core_data->ts_notifier);
+	goodix_ts_power_off(core_data);
 
 	return 0;
 }
